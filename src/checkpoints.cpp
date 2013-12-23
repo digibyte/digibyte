e8041944dd983930f2265e5b6498e81c9e7050a1
zetacoin <zetacoin>
2013-12-23 20:36:37 +0200

    Checkpoint

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -44,11 +44,12 @@ namespace Checkpoints
         (252509, uint256("0x0000000000090c2b77a3247303784289fb6a18752d54e38e96d2b48eac245016"))
         (470201, uint256("0x00000000000102bdfdfc228ee34304f64650825fd1639a1f57a397af854b9df1"))
         (523001, uint256("0x00000000000076412e07ded5bcdf11c0ea6bfcada9e339cb31d312d8e60c3ef8"))
+        (569410, uint256("0x000000000000085bbed51c9196314ee52281428ff5b1d8cade9140efe4b33381"))
         ;
     static const CCheckpointData data = {
         &mapCheckpoints,
-        1386846182, // * UNIX timestamp of last checkpoint block
-        743963,     // * total number of transactions between genesis and last checkpoint
+        1387821515, // * UNIX timestamp of last checkpoint block
+        817182,     // * total number of transactions between genesis and last checkpoint
                     //   (the tx=... number in the SetBestChain debug.log lines)
         2880        // * estimated number of transactions per day after checkpoint
     };
