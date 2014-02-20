707a86aa3b61709196f30278b97de400aafc335d
l0rdicon <xploited.ca@gmail.com>
2014-02-19 20:33:27 -0400

    updated code for switch height on testnet

--- src/main.cpp
+++ src/main.cpp
@@ -902,13 +902,7 @@ unsigned int static GetNextWorkRequired(const CBlockIndex* pindexLast, const CBl
         return nProofOfWorkLimit;
 		
 	//Height that new difficulty takes effect
-	static const int nDifficultySwitchHeight;
-	if(fTestNet) {
-		static const int nDifficultySwitchHeight = 180;
-	} else {
-	static const int nDifficultySwitchHeight = 70000;
-	}
- 	
+	static const int nDifficultySwitchHeight = (fTestNet) 180 : 70000;
 	
  	int nHeight = pindexLast->nHeight + 1;
  	if(nHeight < nDifficultySwitchHeight ) {
