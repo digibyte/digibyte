601a560e33cf1436a204df08a572c87ac26fabf9
myriadcoin <myriadcoin>
2014-03-04 18:11:15 +0200

    Checkpoint

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -39,11 +39,12 @@ namespace Checkpoints
         boost::assign::map_list_of
         ( 0,     uint256("0x00000ffde4c020b5938441a0ea3d314bf619eff0b38f32f78f7583cffa1ea485"))
         ( 9646,  uint256("0x0000000000000b0f1372211861f226a3ec06a27d0a5bf36e4244a982da077e8f"))
+        (27255,  uint256("0x00000000000005112a0debf53703eb3dc4ec2d8d68599c90db71423ea14489b7"))
         ;
     static const CCheckpointData data = {
         &mapCheckpoints,
-        1393411610, // * UNIX timestamp of last checkpoint block
-        16416,      // * total number of transactions between genesis and last checkpoint
+        1393944775, // * UNIX timestamp of last checkpoint block
+        62179,      // * total number of transactions between genesis and last checkpoint
                     //   (the tx=... number in the SetBestChain debug.log lines)
         2880        // * estimated number of transactions per day after checkpoint
     };
