d94343bc9cbbb5d29630b454c76abe0c4a7d2f4d
Administrator <admin@local.host>
2014-06-15 19:05:18 -0700

    Updates for testing setup

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -16,21 +16,21 @@
 
 unsigned int pnSeed[] =
 {
-            0x797DFAD8, 0xA0E6D04A, 0xCACC704A, 0xDC20042E, 0x586D1BC6,
-           0x2124CC36, 0x8CB829C6, 0xB65E4C90, 0xBC32555F, 0x46F09FA2,
+//            0x797DFAD8, 0xA0E6D04A, 0xCACC704A, 0xDC20042E, 0x586D1BC6,
+//           0x2124CC36, 0x8CB829C6, 0xB65E4C90, 0xBC32555F, 0x46F09FA2,
 };
 
 class CMainParams : public CChainParams {
 public:
     CMainParams() {
         // The message start string is designed to be unlikely to occur in normal data.
-        pchMessageStart[0] = 0xfa;
-        pchMessageStart[1] = 0xc3;
-        pchMessageStart[2] = 0xb6;
+        pchMessageStart[0] = 0xf1;
+        pchMessageStart[1] = 0xc4;
+        pchMessageStart[2] = 0xb2;
         pchMessageStart[3] = 0xda;
         vAlertPubKey = ParseHex("04F04441C4757F356290A37C313C3772C5BC5003E898EB2E0CF365795543A7BF690C8BBBFA32EE3A3325477CE2000B7D0453EFBB203329D0F9DF34D5927D022BC9");
-        nDefaultPort = 12024;
-        nRPCPort = 14022;
+        nDefaultPort = 11024;
+        nRPCPort = 11022;
         bnProofOfWorkLimit[ALGO_SHA256D] = CBigNum(~uint256(0) >> 20);
         bnProofOfWorkLimit[ALGO_SCRYPT]  = CBigNum(~uint256(0) >> 20);
         bnProofOfWorkLimit[ALGO_GROESTL] = CBigNum(~uint256(0) >> 20);
@@ -53,9 +53,9 @@ public:
         genesis.hashMerkleRoot = genesis.BuildMerkleTree();
         //genesis.nVersion = BLOCK_VERSION_DEFAULT;
         genesis.nVersion = 1;
-        genesis.nTime    = 1389388394;
+        genesis.nTime    = 1392796564;
         genesis.nBits    = 0x1e0ffff0;
-        genesis.nNonce   = 2447652;
+        genesis.nNonce   = 961533;
         
         //// debug print
         hashGenesisBlock = genesis.GetHash();
@@ -68,14 +68,14 @@ public:
         //printf("%s\n", genesis.hashMerkleRoot.ToString().c_str());
         //printf("%x\n", bnProofOfWorkLimit[ALGO_SHA256D].GetCompact());
         //genesis.print();
-        
-        assert(hashGenesisBlock == uint256("0x7497ea1b465eb39f1c8f507bc877078fe016d6fcb6dfad3a64c98dcc6e1e8496"));
+        assert(hashGenesisBlock == uint256("0xb5dca8039e300198e5fe7cd23bdd1728e2a444af34c447dbd0916fa3430a68c2");
+        //assert(hashGenesisBlock == uint256("0x7497ea1b465eb39f1c8f507bc877078fe016d6fcb6dfad3a64c98dcc6e1e8496"));
         assert(genesis.hashMerkleRoot == uint256("0x72ddd9496b004221ed0557358846d9248ecd4c440ebd28ed901efc18757d0fad"));
 
-        vSeeds.push_back(CDNSSeedData("digibyte.co seed #1", "seed1.digibyte.co"));
-        vSeeds.push_back(CDNSSeedData("hashdragon.com seed #2", "seed2.hashdragon.com"));
+        //vSeeds.push_back(CDNSSeedData("digibyte.co seed #1", "seed1.digibyte.co"));
+        //vSeeds.push_back(CDNSSeedData("hashdragon.com seed #2", "seed2.hashdragon.com"));
 
-        base58Prefixes[PUBKEY_ADDRESS] = 30;
+        base58Prefixes[PUBKEY_ADDRESS] = 127;
         base58Prefixes[SCRIPT_ADDRESS] = 5;
         base58Prefixes[SECRET_KEY] =     128;
 
