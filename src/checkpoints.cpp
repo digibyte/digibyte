ab24002c347f4951b739961a37e0ac9734b2ca61
myriadcoin <myriadcoin>
2014-08-11 20:09:55 +0200

    Checkpoint

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -48,11 +48,12 @@ namespace Checkpoints
         (149540, uint256("0x000000000000061b8f5b8653fe13b5e453347d9386d43d09445ee5e26a8222bb"))
         (348178, uint256("0x0000000000000a410c6aff939087769e757132098fa0b0ce89f11d68f935077d"))
         (431747, uint256("0x000000000000065616abeccd681f7b4d6d7bed06deef0e1a6e70c905edae3692"))
+        (490168, uint256("0x000000000000155ce9845e501617f152127e32c2c468edc643809f9988d30327"))
         ;
     static const CCheckpointData data = {
         &mapCheckpoints,
-        1406033731, // * UNIX timestamp of last checkpoint block
-        1177428,    // * total number of transactions between genesis and last checkpoint
+        1407779009, // * UNIX timestamp of last checkpoint block
+        1296740,    // * total number of transactions between genesis and last checkpoint
                     //   (the tx=... number in the SetBestChain debug.log lines)
         2880        // * estimated number of transactions per day after checkpoint
     };
