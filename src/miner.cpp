9d68a07a3a770b9c9f16c555efc2f5fdae781c41
digibyte <dev@digibyte.co>
2014-08-13 18:27:30 -0600

    Revert "Missing If"
    This reverts commit cde0b0a349c970543d335af05257af7bff13454f.

--- src/miner.cpp
+++ src/miner.cpp
@@ -134,10 +134,6 @@ CBlockTemplate* CreateNewBlock(const CScript& scriptPubKeyIn, int algo)
             error("CreateNewBlock: bad algo");
             return NULL;
     }
-      if (pindexBest->nHeight < multiAlgoDiffChangeTarget && algo != ALGO_SCRYPT) {
-	    error("MultiAlgo is not yet active. Current block height %d, height multialgo becomes active %"PRI64d"", pindexBest->nHeight, multiAlgoDiffChangeTarget);
-            return NULL;
-    }
     
     // Create coinbase tx
     CTransaction txNew;
