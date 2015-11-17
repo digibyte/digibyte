0798c9b7d6d6525218962ddcfc33d81450f96eac
terry <terry@tofugear.com>
2015-11-17 20:11:21 +0800

    change default transaction value to from 0 to 0.1

--- src/wallet.h
+++ src/wallet.h
@@ -28,7 +28,7 @@ extern int64_t nTransactionFee;
 extern bool bSpendZeroConfChange;
 
 // -paytxfee default
-static const int64_t DEFAULT_TRANSACTION_FEE = 0;
+static const int64_t DEFAULT_TRANSACTION_FEE = 0.1;
 // -paytxfee will warn if called with a higher fee than this amount (in satoshis) per KB
 static const int nHighTransactionFeeWarning = 0.01 * COIN;
 
