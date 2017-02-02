57a9d1bd92cfc1805e44d787ea4def3d7ce3d00f
Esotericizm <esotericizm88@gmail.com>
2017-02-02 20:43:13 +1000

    rpc ports

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -32,7 +32,7 @@ class CBaseMainParams : public CBaseChainParams
 public:
     CBaseMainParams()
     {
-        nRPCPort = 8332;
+        nRPCPort = 14022;
     }
 };
 static CBaseMainParams mainParams;
@@ -45,7 +45,7 @@ class CBaseTestNetParams : public CBaseChainParams
 public:
     CBaseTestNetParams()
     {
-        nRPCPort = 18332;
+        nRPCPort = 15022;
         strDataDir = "testnet3";
     }
 };
