f67eeeaeb1fd365ad733284cefb0ba46cd8cdd4f
Esotericizm <esotericizm88@gmail.com>
2017-02-02 20:47:14 +1000

    Signed message

--- src/validation.cpp
+++ src/validation.cpp
@@ -88,7 +88,7 @@ static void CheckBlockIndex(const Consensus::Params& consensusParams);
 /** Constant stuff for coinbase transactions we create: */
 CScript COINBASE_FLAGS;
 
-const std::string strMessageMagic = "Bitcoin Signed Message:\n";
+const std::string strMessageMagic = "DigiByte Signed Message:\n";
 
 // Internal stuff
 namespace {
