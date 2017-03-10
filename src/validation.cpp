1e2ef9b4e36b24262f72e4af271ee0c133b22a75
Esotericizm <esotericizm88@gmail.com>
2017-03-10 14:22:21 +1000

    fix typo

--- src/validation.cpp
+++ src/validation.cpp
@@ -3044,7 +3044,7 @@ bool ContextualCheckBlockHeader(const CBlockHeader& block, CValidationState& sta
     if (block.GetBlockTime() > nAdjustedTime + 2 * 60 * 60)
         return state.Invalid(false, REJECT_INVALID, "time-too-new", "block timestamp too far in the future");
 
-    if(block.nVersion < 3 && IsWitnessEnabled(pindexPrev, consensusParams))
+    if(block.nVersion < VERSIONBITS_TOP_BITS && IsWitnessEnabled(pindexPrev, consensusParams))
     {
             return state.Invalid(false, REJECT_OBSOLETE, strprintf("bad-version(0x%08x)", block.nVersion),
                                  strprintf("rejected nVersion=0x%08x block", block.nVersion));
