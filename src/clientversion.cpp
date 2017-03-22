2eb11f43e10256c35afe5ccf3fa8600b10f46d73
esotericizm <esotericizm88@gmail.com>
2017-03-23 01:21:26 +1000

    change client name

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -13,7 +13,7 @@
  * for both digibyted and digibyte-core, to make it harder for attackers to
  * target servers or GUI users specifically.
  */
-const std::string CLIENT_NAME("Satoshi");
+const std::string CLIENT_NAME("Digitoshi");
 
 /**
  * Client version number
