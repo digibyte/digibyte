84d611fb22904d619573e0e0a0f15362c63e151a
Jared Tate <dev@digibyte.co>
2017-03-27 17:29:31 +0800

    VersionBits Test

--- src/versionbits.h
+++ src/versionbits.h
@@ -13,7 +13,7 @@ static const int32_t VERSIONBITS_LAST_OLD_BLOCK_VERSION = 2;
 /** What bits to set in version for versionbits blocks */
 static const int32_t VERSIONBITS_TOP_BITS = 0x20000000UL;
 /** What bitmask determines whether versionbits is in use */
-static const int32_t VERSIONBITS_TOP_MASK = 0xE0000000UL;
+static const int32_t VERSIONBITS_TOP_MASK = 0xF0000000UL;
 /** Total bits available for versionbits */
 static const int32_t VERSIONBITS_NUM_BITS = 28;
 
