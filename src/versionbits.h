7b54b01ad3d39dc0483c0653e1635274ef3a6b66
Jared Tate <dev@digibyte.co>
2017-03-30 12:01:25 +0800

    Version Test

--- src/versionbits.h
+++ src/versionbits.h
@@ -11,11 +11,11 @@
 /** What block version to use for new blocks (pre versionbits) */
 static const int32_t VERSIONBITS_LAST_OLD_BLOCK_VERSION = 4;
 /** What bits to set in version for versionbits blocks */
-static const int32_t VERSIONBITS_TOP_BITS = 0x20000000UL;
+static const int32_t VERSIONBITS_TOP_BITS = 0x20009000UL;
 /** What bitmask determines whether versionbits is in use */
-static const int32_t VERSIONBITS_TOP_MASK = 0xF0000000UL;
+static const int32_t VERSIONBITS_TOP_MASK = 0xE0000000UL;
 /** Total bits available for versionbits */
-static const int32_t VERSIONBITS_NUM_BITS = 28;
+static const int32_t VERSIONBITS_NUM_BITS = 29;
 
 enum ThresholdState {
     THRESHOLD_DEFINED,
