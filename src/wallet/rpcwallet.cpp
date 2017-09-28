6e012e7ad1293096bae8c0b16884ac8f760b5038
jojoe77777 <jojoe77777@users.noreply.github.com>
2017-09-28 23:59:18 +1000

    Fixed typo

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -2109,7 +2109,7 @@ UniValue encryptwallet(const JSONRPCRequest& request)
             "\nArguments:\n"
             "1. \"passphrase\"    (string) The pass phrase to encrypt the wallet with. It must be at least 1 character, but should be long.\n"
             "\nExamples:\n"
-            "\nEncrypt you wallet\n"
+            "\nEncrypt your wallet\n"
             + HelpExampleCli("encryptwallet", "\"my pass phrase\"") +
             "\nNow set the passphrase to use the wallet, such as for signing or sending digibyte\n"
             + HelpExampleCli("walletpassphrase", "\"my pass phrase\"") +
