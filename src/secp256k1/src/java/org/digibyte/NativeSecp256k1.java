2b47c79c7a99f5829b1b07002697760fb9c0ddfd
DigiByte Team <dev@digibyte.co>
2018-02-15 14:50:35 -0800

    2017

--- src/secp256k1/src/java/org/digibyte/NativeSecp256k1.java
+++ src/secp256k1/src/java/org/digibyte/NativeSecp256k1.java
@@ -1,6 +1,6 @@
 /*
  * Copyright 2013 Google Inc.
- * Copyright 2014-2016 the libsecp256k1 contributors
+ * Copyright 2014-2017 the libsecp256k1 contributors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
