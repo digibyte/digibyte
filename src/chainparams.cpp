fd09c19ddbf27c4c16b12d8cbf7e26bda08c05ce
DigiByte Team <dev@digibyte.co>
2018-02-16 14:46:07 -0800

    Make Future DigiByte Segwit Addresses start with "dgb1" Prefix

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -221,7 +221,7 @@ public:
         base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x88, 0xB2, 0x1E};
         base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x88, 0xAD, 0xE4};
 
-        bech32_hrp = "DGB";
+        bech32_hrp = "dgb";
 
         vFixedSeeds = std::vector<SeedSpec6>(pnSeed6_main, pnSeed6_main + ARRAYLEN(pnSeed6_main));
 
