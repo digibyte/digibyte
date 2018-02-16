7a2bbaf2a8bc98e2824edc571d13716a69eb7356
DigiByte Team <dev@digibyte.co>
2018-02-16 10:49:18 -0800

    Image & Seed Updates

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -206,6 +206,8 @@ public:
         // Note that of those with the service bits flag, most only support a subset of possible options
         vSeeds.emplace_back("seed.digibyte.io");
         vSeeds.emplace_back("seed.digibyteprojects.com");
+        vSeeds.emplace_back("digihash.co");
+        vSeeds.emplace_back("digiexplorer.info");
 
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,30);
