64f6803f310fee2a7f3bb781a6adb5c143d645b9
DigiByte Team <dev@digibyte.co>
2018-02-18 17:32:42 -0800

    Put Back

--- src/consensus/params.h
+++ src/consensus/params.h
@@ -52,7 +52,15 @@ struct BIP9Deployment {
 struct Params {
     uint256 hashGenesisBlock;
     int nSubsidyHalvingInterval;
-
+    /** Block height at which BIP16 becomes active */
+    int BIP16Height;
+    /** Block height and hash at which BIP34 becomes active */
+    int BIP34Height;
+    uint256 BIP34Hash;
+    /** Block height at which BIP65 becomes active */
+    int BIP65Height;
+    /** Block height at which BIP66 becomes active */
+    int BIP66Height;
     
     /**
      * Minimum blocks including miner confirmation of the total of 2016 blocks in a retargeting period,
