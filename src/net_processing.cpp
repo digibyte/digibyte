a1cae5cd76e29ce66babce63e6aae5a128c5d2a9
digicontributer <esotericizm@digibyte.io>
2018-02-20 14:03:06 +1000

    raise getblocks limit

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -1966,7 +1966,7 @@ bool static ProcessMessage(CNode* pfrom, const std::string& strCommand, CDataStr
         // Send the rest of the chain
         if (pindex)
             pindex = chainActive.Next(pindex);
-        int nLimit = 500;
+        int nLimit = 2500;
         LogPrint(BCLog::NET, "getblocks %d to %s limit %d from peer=%d\n", (pindex ? pindex->nHeight : -1), hashStop.IsNull() ? "end" : hashStop.ToString(), nLimit, pfrom->GetId());
         for (; pindex; pindex = chainActive.Next(pindex))
         {
