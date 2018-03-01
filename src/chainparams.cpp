3ce31ce2c03b7d56137bf2b2f02f2d96ad278987
digicontributer <esotericizm@digibyte.io>
2018-03-01 12:48:50 +1000

    use dgbt for bech32 testnet prefix

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -387,7 +387,7 @@ public:
         base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x35, 0x87, 0xCF};
         base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x35, 0x83, 0x94};
 
-        bech32_hrp = "tb";
+        bech32_hrp = "dgbt";
 
         vFixedSeeds = std::vector<SeedSpec6>(pnSeed6_test, pnSeed6_test + ARRAYLEN(pnSeed6_test));
 
