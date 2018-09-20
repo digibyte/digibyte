d340fd06edf7e644bdf16bc0327559f16b8eb664
DigiByte <info@digibyte.io>
2018-09-20 16:54:00 -0500

    Fix Priv key import

--- src/key_io.cpp
+++ src/key_io.cpp
@@ -135,7 +135,7 @@ CKey DecodeSecret(const std::string& str)
     CKey key;
     std::vector<unsigned char> data;
     if (DecodeBase58Check(str, data)) {
-        if(str.at(0) == '6') {
+        if(str.at(0) == '6'|| str.at(0) == 'Q') {
             const std::vector<unsigned char>& privkey_prefix = Params().Base58Prefix(CChainParams::SECRET_KEY_OLD);
             if ((data.size() == 32 + privkey_prefix.size() || (data.size() == 33 + privkey_prefix.size() && data.back() == 1)) &&
                 std::equal(privkey_prefix.begin(), privkey_prefix.end(), data.begin())) {
