a7469ae309dc0d17f05cc2fbba5ada21fdca26db
MentalCollatz <MentalCollatz@users.noreply.github.com>
2018-11-17 15:31:42 -0800

    Odo v2

--- src/crypto/odocrypt.cpp
+++ src/crypto/odocrypt.cpp
@@ -42,11 +42,21 @@ struct OdoRandom
     uint64_t addend;
 };
 
-template<class T>
-void InvertMapping(T res[], const T mapping[], size_t size)
+void RandomizePbox(OdoRandom& r, uint64_t pbox[OdoCrypt::STATE_SIZE][OdoCrypt::STATE_SIZE])
 {
-    for (size_t i = 0; i < size; i++)
-        res[mapping[i]] = i;
+    for (int i = 0; i < OdoCrypt::STATE_SIZE; i++)
+    for (int j = 0; j < OdoCrypt::STATE_SIZE; j++)
+        pbox[i][j] = 0;
+    for (int i = 0; i < OdoCrypt::WORD_BITS; i++)
+    {
+        int cycle[OdoCrypt::STATE_SIZE + 1];
+        for (size_t j = 0; j < OdoCrypt::STATE_SIZE; j++)
+            cycle[j] = j;
+        r.Shuffle(cycle, OdoCrypt::STATE_SIZE);
+        cycle[OdoCrypt::STATE_SIZE] = cycle[0];
+        for (size_t j = 0; j < OdoCrypt::STATE_SIZE; j++)
+            pbox[cycle[j]][cycle[j+1]] |= (uint64_t)1 << i;
+    }
 }
 
 OdoCrypt::OdoCrypt(uint32_t key)
@@ -54,330 +64,206 @@ OdoCrypt::OdoCrypt(uint32_t key)
     OdoRandom r(key);
 
     // Randomize each s-box
-    for (int i = 0; i < STATE_SIZE; i++)
+    for (int i = 0; i < SBOX_COUNT; i++)
     {
-        for (int j = 0; j < SBOX_VALUES; j++)
-            Sbox[i][j] = j;
-        r.Shuffle(Sbox[i], SBOX_VALUES);
-        InvertMapping(InvSbox[i], Sbox[i], SBOX_VALUES);
+        for (int j = 0; j < 1 << SMALL_SBOX_WIDTH; j++)
+            Sbox1[i][j] = j;
+        r.Shuffle(Sbox1[i], 1 << SMALL_SBOX_WIDTH);
+
+        // Duplicate half of the large s-boxes, in order to take advantage of
+        // dual-port ram
+        if (i & 1)
+            std::copy(Sbox2[i-1], Sbox2[i-1] + (1 << LARGE_SBOX_WIDTH), Sbox2[i]);
+        else
+        {
+            for (int j = 0; j < 1 << LARGE_SBOX_WIDTH; j++)
+                Sbox2[i][j] = j;
+            r.Shuffle(Sbox2[i], 1 << LARGE_SBOX_WIDTH);
+        }
     }
 
     // Randomize each p-box
-    for (int i = 0; i < SBOX_WIDTH; i++)
-    {
-        int cycle[STATE_SIZE];
-        for (int j = 0; j < STATE_SIZE; j++)
-            cycle[j] = j;
-        r.Shuffle(cycle, STATE_SIZE);
-        for (int j = 1; j < STATE_SIZE; j++)
-            Pbox[i][cycle[j-1]] = cycle[j];
-        Pbox[i][cycle[STATE_SIZE-1]] = cycle[0];
-        InvertMapping(InvPbox[i], Pbox[i], STATE_SIZE);
-    }
+    RandomizePbox(r, Pbox1);
+    RandomizePbox(r, Pbox2);
+
     // Randomize each round key
     for (int i = 0; i < ROUNDS; i++)
-        RoundKey[i] = r.Next(1 << ROUNDKEY_BITS);
+        RoundKey[i] = r.Next(1 << STATE_SIZE);
+
+    // Randomize rotations
+    for (int i = 0; i < STATE_SIZE; i++)
+    {
+        int bits[WORD_BITS];
+        for (int j = 0; j < WORD_BITS; j++)
+            bits[j] = j;
+        r.Shuffle(bits, WORD_BITS);
+
+        int sum = 0;
+        for (int j = 0; j < ROTATION_COUNT; j++)
+            sum += bits[j];
+        // sum must be odd
+        if ((sum % 2) == 0)
+        {
+            for (int j = ROTATION_COUNT; ; j++)
+            {
+                if ((bits[j] + bits[0]) % 2)
+                {
+                    bits[0] = bits[j];
+                    break;
+                }
+            }
+        }
+        std::copy(bits, bits + ROTATION_COUNT, Rotations[i]);
+    }
 }
 
 void OdoCrypt::Encrypt(char cipher[DIGEST_SIZE], const char plain[DIGEST_SIZE]) const
 {
-    uint8_t state[STATE_SIZE];
+    uint64_t state[STATE_SIZE];
     Unpack(state, plain);
+    PreMix(state);
     for (int round = 0; round < ROUNDS; round++)
     {
-        ApplySbox(state, Sbox);
-        ApplyPbox(state, Pbox);
+        ApplyPbox(state, Pbox1);
+        ApplySboxes(state, Sbox1, Sbox2);
+        ApplyPbox(state, Pbox2);
+        ApplyRotations(state, Rotations);
         ApplyRoundKey(state, RoundKey[round]);
     }
     Pack(state, cipher);
 }
 
-void OdoCrypt::EncryptBitsliced(char cipher[64][DIGEST_SIZE], const char plain[64][DIGEST_SIZE]) const
+template<class T, size_t sz1, size_t sz2>
+void InvertMapping(T (&res)[sz1][sz2], const T (&mapping)[sz1][sz2])
 {
-    uint64_t state[DIGEST_BITS];
-    UnpackBitsliced(state, plain);
-    for (int round = 0; round < ROUNDS; round++)
-    {
-        for (int i = 0; i < STATE_SIZE; i++)
-            ApplySboxBitsliced(state + SBOX_WIDTH*i, Sbox[i]);
-        ApplyPboxBitsliced(state, Pbox);
-        ApplyRoundKeyBitsliced(state, RoundKey[round]);
-    }
-    PackBitsliced(state, cipher);
+    for (size_t i = 0; i < sz1; i++)
+    for (size_t j = 0; j < sz2; j++)
+        res[i][mapping[i][j]] = j;
+}
+
+template<class T, size_t sz>
+void Transpose(T (&res)[sz][sz], const T (&orig)[sz][sz])
+{
+    for (size_t i = 0; i < sz; i++)
+    for (size_t j = 0; j < sz; j++)
+        res[i][j] = orig[j][i];
 }
 
 void OdoCrypt::Decrypt(char plain[DIGEST_SIZE], const char cipher[DIGEST_SIZE]) const
 {
-    uint8_t state[STATE_SIZE];
+    uint8_t invSbox1[SBOX_COUNT][1 << SMALL_SBOX_WIDTH];
+    uint16_t invSbox2[SBOX_COUNT][1 << LARGE_SBOX_WIDTH];
+    uint64_t invPbox1[STATE_SIZE][STATE_SIZE];
+    uint64_t invPbox2[STATE_SIZE][STATE_SIZE];
+
+    InvertMapping(invSbox1, Sbox1);
+    InvertMapping(invSbox2, Sbox2);
+    Transpose(invPbox1, Pbox1);
+    Transpose(invPbox2, Pbox2);
+
+    uint64_t state[STATE_SIZE];
     Unpack(state, cipher);
     for (int round = ROUNDS-1; round >= 0; round--)
     {
         ApplyRoundKey(state, RoundKey[round]);
-        ApplyPbox(state, InvPbox);
-        ApplySbox(state, InvSbox);
+        for (int i = 0; i < WORD_BITS-1; i++)
+            ApplyRotations(state, Rotations);
+        ApplyPbox(state, invPbox2);
+        ApplySboxes(state, invSbox1, invSbox2);
+        ApplyPbox(state, invPbox1);
     }
+    PreMix(state);
     Pack(state, plain);
 }
 
-void OdoCrypt::Unpack(uint8_t state[STATE_SIZE], const char bytes[DIGEST_SIZE])
+void OdoCrypt::Unpack(uint64_t state[STATE_SIZE], const char bytes[DIGEST_SIZE])
 {
     std::fill(state, state+STATE_SIZE, 0);
-    unsigned int pos = 0;
     for (int i = 0; i < STATE_SIZE; i++)
     {
-        for (int j = 0; j < SBOX_WIDTH; j++)
+        for (int j = 0; j < 8; j++)
         {
-            uint8_t bit = (bytes[pos / 8] >> (pos % 8)) & 1;
-            state[i] |= bit << j;
-            pos++;
+            state[i] |= (uint64_t)(uint8_t)bytes[8*i + j] << (8*j);
         }
     }
 }
 
-void OdoCrypt::Pack(const uint8_t state[STATE_SIZE], char bytes[DIGEST_SIZE])
+void OdoCrypt::Pack(const uint64_t state[STATE_SIZE], char bytes[DIGEST_SIZE])
 {
     std::fill(bytes, bytes+DIGEST_SIZE, 0);
-    unsigned int pos = 0;
     for (int i = 0; i < STATE_SIZE; i++)
     {
-        for (int j = 0; j < SBOX_WIDTH; j++)
+        for (int j = 0; j < 8; j++)
         {
-            uint8_t bit = (state[i] >> j) & 1;
-            bytes[pos / 8] |= bit << (pos % 8);
-            pos++;
+            bytes[8*i + j] = (state[i] >> (8*j)) & 0xff;
         }
     }
 }
 
-void OdoCrypt::ApplySbox(uint8_t state[STATE_SIZE], const uint8_t sbox[STATE_SIZE][SBOX_VALUES])
+void OdoCrypt::PreMix(uint64_t state[STATE_SIZE])
 {
+    uint64_t total = 0;
     for (int i = 0; i < STATE_SIZE; i++)
-        state[i] = sbox[i][state[i]];
+        total ^= state[i];
+    total ^= total >> 32;
+    for (int i = 0; i < STATE_SIZE; i++)
+        state[i] ^= total;
 }
 
-void OdoCrypt::ApplyPbox(uint8_t state[STATE_SIZE], const int pbox[SBOX_WIDTH][STATE_SIZE])
+void OdoCrypt::ApplySboxes(
+    uint64_t state[STATE_SIZE],
+    const uint8_t sbox1[SBOX_COUNT][1 << SMALL_SBOX_WIDTH],
+    const uint16_t sbox2[SBOX_COUNT][1 << LARGE_SBOX_WIDTH])
 {
-    uint8_t next[STATE_SIZE] = {};
-    for (int i = 0; i < SBOX_WIDTH; i++)
+    const static uint64_t MASK1 = (1 << SMALL_SBOX_WIDTH) - 1;
+    const static uint64_t MASK2 = (1 << LARGE_SBOX_WIDTH) - 1;
+
+    int sboxIndex = 0;
+    for (int i = 0; i < STATE_SIZE; i++)
+    {
+        uint64_t next = 0;
+        int pos = 0;
+        for (int j = 0; j < SBOX_COUNT / STATE_SIZE; j++)
+        {
+            next |= (uint64_t)sbox1[sboxIndex][(state[i] >> pos) & MASK1] << pos;
+            pos += SMALL_SBOX_WIDTH;
+            next |= (uint64_t)sbox2[sboxIndex][(state[i] >> pos) & MASK2] << pos;
+            pos += LARGE_SBOX_WIDTH;
+            sboxIndex++;
+        }
+        state[i] = next;
+    }
+}
+
+void OdoCrypt::ApplyPbox(uint64_t state[STATE_SIZE], const uint64_t pbox[STATE_SIZE][STATE_SIZE])
+{
+    uint64_t next[STATE_SIZE] = {};
+    for (int i = 0; i < STATE_SIZE; i++)
     for (int j = 0; j < STATE_SIZE; j++)
     {
-        next[pbox[i][j]] |= state[j] & (1<<i);
+        next[j] |= state[i] & pbox[i][j];
     }
     std::copy(next, next+STATE_SIZE, state);
 }
 
-void OdoCrypt::ApplyRoundKey(uint8_t state[STATE_SIZE], int roundKey)
+inline uint64_t Rot(uint64_t x, int r)
 {
-    for (int i = 0; i < ROUNDKEY_BITS; i++)
+    return r == 0 ? x : (x << r) ^ (x >> (64-r));
+}
+
+void OdoCrypt::ApplyRotations(uint64_t state[STATE_SIZE], const int rotations[STATE_SIZE][ROTATION_COUNT])
+{
+    for (int i = 0; i < STATE_SIZE; i++)
+    {
+        uint64_t next = 0;
+        for (int j = 0; j < ROTATION_COUNT; j++)
+            next ^= Rot(state[i], rotations[i][j]);
+        state[i] = next;
+    }
+}
+
+void OdoCrypt::ApplyRoundKey(uint64_t state[STATE_SIZE], int roundKey)
+{
+    for (int i = 0; i < STATE_SIZE; i++)
         state[i] ^= (roundKey >> i) & 1;
 }
-
-void OdoCrypt::UnpackBitsliced(uint64_t state[DIGEST_BITS], const char bytes[64][DIGEST_SIZE])
-{
-    std::fill(state, state+DIGEST_BITS, 0);
-    for (int i = 0; i < 64; i++)
-    {
-        for (int j = 0; j < DIGEST_SIZE; j++)
-        {
-            for (int k = 0; k < 8; k++)
-            {
-                uint64_t bit = (bytes[i][j] >> k) & 1;
-                state[8*j+k] |= bit << i;
-            }
-        }
-    }
-}
-
-void OdoCrypt::PackBitsliced(uint64_t state[DIGEST_BITS], char bytes[64][DIGEST_SIZE])
-{
-    for (int i = 0; i < 64; i++)
-        std::fill(bytes[i], bytes[i]+DIGEST_SIZE, 0);
-    for (int i = 0; i < 64; i++)
-    {
-        for (int j = 0; j < DIGEST_SIZE; j++)
-        {
-            for (int k = 0; k < 8; k++)
-            {
-                uint8_t bit = (state[8*j+k] >> i) & 1;
-                bytes[i][j] |= bit << k;
-            }
-        }
-    }
-}
-
-#define UNROLLED 1
-void OdoCrypt::ApplySboxBitsliced(uint64_t state[SBOX_WIDTH], const uint8_t sbox[SBOX_VALUES])
-{
-    uint64_t data[SBOX_VALUES];
-
-    // demux
-    if (!UNROLLED)
-    {
-        data[0] = -1;
-        for (int i = 0; i < SBOX_WIDTH; i++)
-        {
-            int shift = 1 << i;
-            for (int j = 0; j < shift; j++)
-            {
-                data[j+shift] = data[j] & state[i];
-                data[j] &= ~state[i];
-            }
-        }
-    }
-    else
-    {
-        uint64_t lo[8] = {
-            ~state[0] & ~state[1] & ~state[2],
-             state[0] & ~state[1] & ~state[2],
-            ~state[0] &  state[1] & ~state[2],
-             state[0] &  state[1] & ~state[2],
-            ~state[0] & ~state[1] &  state[2],
-             state[0] & ~state[1] &  state[2],
-            ~state[0] &  state[1] &  state[2],
-             state[0] &  state[1] &  state[2],
-        };
-        uint64_t hi[8] = {
-            ~state[3] & ~state[4] & ~state[5],
-             state[3] & ~state[4] & ~state[5],
-            ~state[3] &  state[4] & ~state[5],
-             state[3] &  state[4] & ~state[5],
-            ~state[3] & ~state[4] &  state[5],
-             state[3] & ~state[4] &  state[5],
-            ~state[3] &  state[4] &  state[5],
-             state[3] &  state[4] &  state[5],
-        };
-        data[0] = lo[0] & hi[0];
-        data[1] = lo[1] & hi[0];
-        data[2] = lo[2] & hi[0];
-        data[3] = lo[3] & hi[0];
-        data[4] = lo[4] & hi[0];
-        data[5] = lo[5] & hi[0];
-        data[6] = lo[6] & hi[0];
-        data[7] = lo[7] & hi[0];
-        data[8] = lo[0] & hi[1];
-        data[9] = lo[1] & hi[1];
-        data[10] = lo[2] & hi[1];
-        data[11] = lo[3] & hi[1];
-        data[12] = lo[4] & hi[1];
-        data[13] = lo[5] & hi[1];
-        data[14] = lo[6] & hi[1];
-        data[15] = lo[7] & hi[1];
-        data[16] = lo[0] & hi[2];
-        data[17] = lo[1] & hi[2];
-        data[18] = lo[2] & hi[2];
-        data[19] = lo[3] & hi[2];
-        data[20] = lo[4] & hi[2];
-        data[21] = lo[5] & hi[2];
-        data[22] = lo[6] & hi[2];
-        data[23] = lo[7] & hi[2];
-        data[24] = lo[0] & hi[3];
-        data[25] = lo[1] & hi[3];
-        data[26] = lo[2] & hi[3];
-        data[27] = lo[3] & hi[3];
-        data[28] = lo[4] & hi[3];
-        data[29] = lo[5] & hi[3];
-        data[30] = lo[6] & hi[3];
-        data[31] = lo[7] & hi[3];
-        data[32] = lo[0] & hi[4];
-        data[33] = lo[1] & hi[4];
-        data[34] = lo[2] & hi[4];
-        data[35] = lo[3] & hi[4];
-        data[36] = lo[4] & hi[4];
-        data[37] = lo[5] & hi[4];
-        data[38] = lo[6] & hi[4];
-        data[39] = lo[7] & hi[4];
-        data[40] = lo[0] & hi[5];
-        data[41] = lo[1] & hi[5];
-        data[42] = lo[2] & hi[5];
-        data[43] = lo[3] & hi[5];
-        data[44] = lo[4] & hi[5];
-        data[45] = lo[5] & hi[5];
-        data[46] = lo[6] & hi[5];
-        data[47] = lo[7] & hi[5];
-        data[48] = lo[0] & hi[6];
-        data[49] = lo[1] & hi[6];
-        data[50] = lo[2] & hi[6];
-        data[51] = lo[3] & hi[6];
-        data[52] = lo[4] & hi[6];
-        data[53] = lo[5] & hi[6];
-        data[54] = lo[6] & hi[6];
-        data[55] = lo[7] & hi[6];
-        data[56] = lo[0] & hi[7];
-        data[57] = lo[1] & hi[7];
-        data[58] = lo[2] & hi[7];
-        data[59] = lo[3] & hi[7];
-        data[60] = lo[4] & hi[7];
-        data[61] = lo[5] & hi[7];
-        data[62] = lo[6] & hi[7];
-        data[63] = lo[7] & hi[7];
-    }
-
-    // permute
-    uint64_t permuted[SBOX_VALUES];
-    for (int i = 0; i < SBOX_VALUES; i++)
-        permuted[sbox[i]] = data[i];
-
-    // mux
-    if (!UNROLLED)
-    {
-        for (int i = SBOX_WIDTH-1; i >= 0; i--)
-        {
-            state[i] = 0;
-            int shift = 1 << i;
-            for (int j = 0; j < shift; j++)
-            {
-                state[i] |= permuted[j+shift];
-                permuted[j] |= permuted[j+shift];
-            }
-        }
-    }
-    else
-    {
-        uint64_t lo[8] = {
-            0,
-            permuted[1] | permuted[ 9] | permuted[17] | permuted[25] | permuted[33] | permuted[41] | permuted[49] | permuted[57],
-            permuted[2] | permuted[10] | permuted[18] | permuted[26] | permuted[34] | permuted[42] | permuted[50] | permuted[58],
-            permuted[3] | permuted[11] | permuted[19] | permuted[27] | permuted[35] | permuted[43] | permuted[51] | permuted[59],
-            permuted[4] | permuted[12] | permuted[20] | permuted[28] | permuted[36] | permuted[44] | permuted[52] | permuted[60],
-            permuted[5] | permuted[13] | permuted[21] | permuted[29] | permuted[37] | permuted[45] | permuted[53] | permuted[61],
-            permuted[6] | permuted[14] | permuted[22] | permuted[30] | permuted[38] | permuted[46] | permuted[54] | permuted[62],
-            permuted[7] | permuted[15] | permuted[23] | permuted[31] | permuted[39] | permuted[47] | permuted[55] | permuted[63],
-        };
-        uint64_t hi[8] = {
-            0,
-            permuted[ 8] | permuted[ 9] | permuted[10] | permuted[11] | permuted[12] | permuted[13] | permuted[14] | permuted[15],
-            permuted[16] | permuted[17] | permuted[18] | permuted[19] | permuted[20] | permuted[21] | permuted[22] | permuted[23],
-            permuted[24] | permuted[25] | permuted[26] | permuted[27] | permuted[28] | permuted[29] | permuted[30] | permuted[31],
-            permuted[32] | permuted[33] | permuted[34] | permuted[35] | permuted[36] | permuted[37] | permuted[38] | permuted[39],
-            permuted[40] | permuted[41] | permuted[42] | permuted[43] | permuted[44] | permuted[45] | permuted[46] | permuted[47],
-            permuted[48] | permuted[49] | permuted[50] | permuted[51] | permuted[52] | permuted[53] | permuted[54] | permuted[55],
-            permuted[56] | permuted[57] | permuted[58] | permuted[59] | permuted[60] | permuted[61] | permuted[62] | permuted[63],
-        };
-        state[0] = lo[1] | lo[3] | lo[5] | lo[7];
-        state[1] = lo[2] | lo[3] | lo[6] | lo[7];
-        state[2] = lo[4] | lo[5] | lo[6] | lo[7];
-        state[3] = hi[1] | hi[3] | hi[5] | hi[7];
-        state[4] = hi[2] | hi[3] | hi[6] | hi[7];
-        state[5] = hi[4] | hi[5] | hi[6] | hi[7];
-    }
-}
-
-void OdoCrypt::ApplyPboxBitsliced(uint64_t state[DIGEST_BITS], const int pbox[SBOX_WIDTH][STATE_SIZE])
-{
-    uint64_t nextState[DIGEST_BITS];
-    for (int i = 0; i < SBOX_WIDTH; i++)
-    {
-        for (int j = 0; j < STATE_SIZE; j++)
-        {
-            nextState[SBOX_WIDTH*pbox[i][j] + i] = state[SBOX_WIDTH*j + i];
-        }
-    }
-    std::copy(nextState, nextState + DIGEST_BITS, state);
-}
-
-void OdoCrypt::ApplyRoundKeyBitsliced(uint64_t state[DIGEST_BITS], int roundKey)
-{
-    for (int i = 0; i < ROUNDKEY_BITS; i++)
-        state[SBOX_WIDTH*i] ^= -(uint64_t)((roundKey >> i) & 1);
-}
-
--- src/crypto/odocrypt.h
+++ src/crypto/odocrypt.h
@@ -2,6 +2,7 @@
 // Copyright (c) 2009-2018 The DigiByte developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #ifndef ODO_CRYPT
 #define ODO_CRYPT
 
@@ -10,48 +11,52 @@
 class OdoCrypt
 {
 public:
-    const static int DIGEST_SIZE = 81;
-    const static int ROUNDS = 96;
-    const static int ROUNDKEY_BITS = 8;
-    const static int SBOX_WIDTH = 6;
+    const static int DIGEST_SIZE = 80;
+    const static int ROUNDS = 84;
+    const static int SMALL_SBOX_WIDTH = 6;
+    const static int LARGE_SBOX_WIDTH = 10;
+    const static int ROTATION_COUNT = 5;
+    const static int WORD_BITS = 64;
     const static int DIGEST_BITS = 8*DIGEST_SIZE;
-    const static int SBOX_VALUES = 1 << SBOX_WIDTH;
-    const static int STATE_SIZE = DIGEST_BITS / SBOX_WIDTH;
+    const static int SBOX_COUNT = DIGEST_BITS / (SMALL_SBOX_WIDTH + LARGE_SBOX_WIDTH);
+    const static int STATE_SIZE = DIGEST_BITS / WORD_BITS;
 
     OdoCrypt(uint32_t key);
 
     void Encrypt(char cipher[DIGEST_SIZE], const char plain[DIGEST_SIZE]) const;
 
-    // Encrypt 64 messages simultaneously
-    void EncryptBitsliced(char cipher[64][DIGEST_SIZE], const char plain[64][DIGEST_SIZE]) const;
-
     // test-only, proves that this really is a permutation function
     void Decrypt(char plain[DIGEST_SIZE], const char cipher[DIGEST_SIZE]) const;
 
 private:
-    uint8_t Sbox[STATE_SIZE][SBOX_VALUES];
-    uint8_t InvSbox[STATE_SIZE][SBOX_VALUES];
-    int Pbox[SBOX_WIDTH][STATE_SIZE];
-    int InvPbox[SBOX_WIDTH][STATE_SIZE];
-    uint8_t RoundKey[ROUNDS];
+    uint8_t Sbox1[SBOX_COUNT][1 << SMALL_SBOX_WIDTH];
+    uint16_t Sbox2[SBOX_COUNT][1 << LARGE_SBOX_WIDTH];
+    uint64_t Pbox1[STATE_SIZE][STATE_SIZE];
+    uint64_t Pbox2[STATE_SIZE][STATE_SIZE];
+    int Rotations[STATE_SIZE][ROTATION_COUNT];
+    uint16_t RoundKey[ROUNDS];
 
     // Pack/unpack bytes into internal state
-    static void Unpack(uint8_t state[STATE_SIZE], const char bytes[DIGEST_SIZE]);
-    static void Pack(const uint8_t state[STATE_SIZE], char bytes[DIGEST_SIZE]);
+    static void Unpack(uint64_t state[STATE_SIZE], const char bytes[DIGEST_SIZE]);
+    static void Pack(const uint64_t state[STATE_SIZE], char bytes[DIGEST_SIZE]);
 
-    // Apply the appropriate S-boxes or P-boxes to the state
-    static void ApplySbox(uint8_t state[STATE_SIZE], const uint8_t sbox[STATE_SIZE][SBOX_VALUES]);
-    static void ApplyPbox(uint8_t state[STATE_SIZE], const int pbox[SBOX_WIDTH][STATE_SIZE]);
+    // Pre-mix the bits.  Involution.
+    static void PreMix(uint64_t state[STATE_SIZE]);
 
-    // Apply round key. Involution.
-    static void ApplyRoundKey(uint8_t state[STATE_SIZE], int roundKey);
+    // Non-linear substitution.
+    static void ApplySboxes(
+        uint64_t state[STATE_SIZE],
+        const uint8_t sbox1[SBOX_COUNT][1 << SMALL_SBOX_WIDTH],
+        const uint16_t sbox2[SBOX_COUNT][1 << LARGE_SBOX_WIDTH]);
 
-    // Bitsliced versions of the above
-    static void UnpackBitsliced(uint64_t state[DIGEST_BITS], const char bytes[64][DIGEST_SIZE]);
-    static void PackBitsliced(uint64_t state[DIGEST_BITS], char bytes[64][DIGEST_SIZE]);
-    static void ApplySboxBitsliced(uint64_t state[SBOX_WIDTH], const uint8_t sbox[SBOX_VALUES]);
-    static void ApplyPboxBitsliced(uint64_t state[DIGEST_BITS], const int pbox[SBOX_WIDTH][STATE_SIZE]);
-    static void ApplyRoundKeyBitsliced(uint64_t state[DIGEST_BITS], int roundKey);
+    // Move bits between words.
+    static void ApplyPbox(uint64_t state[STATE_SIZE], const uint64_t pbox[STATE_SIZE][STATE_SIZE]);
+
+    // Linear mix step.
+    static void ApplyRotations(uint64_t state[STATE_SIZE], const int rotations[STATE_SIZE][ROTATION_COUNT]);
+
+    // Add round key. Involution.
+    static void ApplyRoundKey(uint64_t state[STATE_SIZE], int roundKey);
 };
 
 #endif
