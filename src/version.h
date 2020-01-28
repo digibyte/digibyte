222d15db82b922e779ecb166f5fcf7e9b97291f3
digibyte <info@digibyte.io>
2020-01-28 12:24:07 -0600

    v8.19.0 Image, Proto & Version Bump

--- src/version.h
+++ src/version.h
@@ -10,7 +10,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70017;
+static const int PROTOCOL_VERSION = 70019;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
