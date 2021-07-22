30034e068492f27da994b4d56d01d01aa121cc8f
GitHubdigicontributerdigicontributerdigicontributerdigicontributerdigicontributer <noreply@github.comesot@digibyte.ioesot@digibyte.ioesot@digibyte.ioesot@digibyte.ioesot@digibyte.io>
2021-07-22 21:51:53 +0800

    Merge 8.22: src/noui.h: Merged file with upstream

--- src/noui.h
+++ src/noui.h
@@ -1,10 +1,29 @@
-// Copyright (c) 2013-2014 The DigiByte Core developers
+// Copyright (c) 2013-2020 The Bitcoin Core developers
+// Copyright (c) 2014-2020 The DigiByte Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #ifndef DIGIBYTE_NOUI_H
 #define DIGIBYTE_NOUI_H
 
-extern void noui_connect();
+#include <string>
+
+struct bilingual_str;
+
+/** Non-GUI handler, which logs and prints messages. */
+bool noui_ThreadSafeMessageBox(const bilingual_str& message, const std::string& caption, unsigned int style);
+/** Non-GUI handler, which logs and prints questions. */
+bool noui_ThreadSafeQuestion(const bilingual_str& /* ignored interactive message */, const std::string& message, const std::string& caption, unsigned int style);
+/** Non-GUI handler, which only logs a message. */
+void noui_InitMessage(const std::string& message);
+
+/** Connect all digibyted signal handlers */
+void noui_connect();
+
+/** Redirect all digibyted signal handlers to LogPrintf. Used to check or suppress output during test runs that produce expected errors */
+void noui_test_redirect();
+
+/** Reconnects the regular Non-GUI handlers after having used noui_test_redirect */
+void noui_reconnect();
 
 #endif // DIGIBYTE_NOUI_H
