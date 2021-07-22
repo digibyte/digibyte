d11cbdd0a0e1da3c7949fa695452a92fb3ed52aa
GitHubdigicontributerdigicontributerdigicontributerdigicontributerdigicontributer <noreply@github.comesot@digibyte.ioesot@digibyte.ioesot@digibyte.ioesot@digibyte.ioesot@digibyte.io>
2021-07-22 21:51:53 +0800

    Merge 8.22: depends/packages/native_mac_alias.mk: Merged file with upstream

--- depends/packages/native_mac_alias.mk
+++ depends/packages/native_mac_alias.mk
@@ -1,15 +1,15 @@
 package=native_mac_alias
-$(package)_version=2.0.7
+$(package)_version=2.2.0
 $(package)_download_path=https://github.com/al45tair/mac_alias/archive/
 $(package)_file_name=v$($(package)_version).tar.gz
-$(package)_sha256_hash=6f606d3b6bccd2112aeabf1a063f5b5ece87005a5d7e97c8faca23b916e88838
-$(package)_install_libdir=$(build_prefix)/lib/python/dist-packages
+$(package)_sha256_hash=421e6d7586d1f155c7db3e7da01ca0dacc9649a509a253ad7077b70174426499
+$(package)_install_libdir=$(build_prefix)/lib/python3/dist-packages
 
 define $(package)_build_cmds
-    python setup.py build
+    python3 setup.py build
 endef
 
 define $(package)_stage_cmds
     mkdir -p $($(package)_install_libdir) && \
-    python setup.py install --root=$($(package)_staging_dir) --prefix=$(build_prefix) --install-lib=$($(package)_install_libdir)
+    python3 setup.py install --root=$($(package)_staging_dir) --prefix=$(build_prefix) --install-lib=$($(package)_install_libdir)
 endef
