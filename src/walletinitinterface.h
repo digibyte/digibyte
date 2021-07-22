46ab944eee00a9b2e6a6d2c1a9a5494953dde941
GitHubdigicontributerdigicontributerdigicontributerdigicontributerdigicontributer <noreply@github.comesot@digibyte.ioesot@digibyte.ioesot@digibyte.ioesot@digibyte.ioesot@digibyte.io>
2021-07-22 21:51:53 +0800

    Merge 8.22: src/walletinitinterface.h: Merged file with upstream

--- src/walletinitinterface.h
+++ src/walletinitinterface.h
@@ -1,38 +1,29 @@
-// Copyright (c) 2009-2019 The Bitcoin Core developers
-// Copyright (c) 2014-2019 The DigiByte Core developers
+// Copyright (c) 2009-2020 The Bitcoin Core developers
+// Copyright (c) 2014-2020 The DigiByte Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #ifndef DIGIBYTE_WALLETINITINTERFACE_H
 #define DIGIBYTE_WALLETINITINTERFACE_H
 
-#include <string>
+class ArgsManager;
 
-class CScheduler;
-class CRPCTable;
+struct NodeContext;
 
 class WalletInitInterface {
 public:
+    /** Is the wallet component enabled */
+    virtual bool HasWalletSupport() const = 0;
     /** Get wallet help string */
-    virtual void AddWalletOptions() const = 0;
+    virtual void AddWalletOptions(ArgsManager& argsman) const = 0;
     /** Check wallet parameter interaction */
     virtual bool ParameterInteraction() const = 0;
-    /** Register wallet RPC*/
-    virtual void RegisterRPC(CRPCTable &) const = 0;
-    /** Verify wallets */
-    virtual bool Verify() const = 0;
-    /** Open wallets*/
-    virtual bool Open() const = 0;
-    /** Start wallets*/
-    virtual void Start(CScheduler& scheduler) const = 0;
-    /** Flush Wallets*/
-    virtual void Flush() const = 0;
-    /** Stop Wallets*/
-    virtual void Stop() const = 0;
-    /** Close wallets */
-    virtual void Close() const = 0;
+    /** Add wallets that should be opened to list of chain clients. */
+    virtual void Construct(NodeContext& node) const = 0;
 
     virtual ~WalletInitInterface() {}
 };
+
+extern const WalletInitInterface& g_wallet_init_interface;
 
 #endif // DIGIBYTE_WALLETINITINTERFACE_H
