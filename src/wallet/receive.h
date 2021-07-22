458a92fbafc71ada4b2623a351dd70cf72a0855e
GitHubdigicontributerdigicontributerdigicontributerdigicontributerdigicontributer <noreply@github.comesot@digibyte.ioesot@digibyte.ioesot@digibyte.ioesot@digibyte.ioesot@digibyte.io>
2021-07-22 21:51:53 +0800

    Merge 8.22: src/wallet/receive.h: Added and modified file from upstream

---
+++ src/wallet/receive.h
@@ -0,0 +1,20 @@
+// Copyright (c) 2021 The DigiByte Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef DIGIBYTE_WALLET_RECEIVE_H
+#define DIGIBYTE_WALLET_RECEIVE_H
+
+#include <amount.h>
+#include <wallet/ismine.h>
+#include <wallet/transaction.h>
+#include <wallet/wallet.h>
+
+struct COutputEntry
+{
+    CTxDestination destination;
+    CAmount amount;
+    int vout;
+};
+
+#endif // DIGIBYTE_WALLET_RECEIVE_H
