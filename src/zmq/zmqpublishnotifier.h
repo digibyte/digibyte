37ecffe211173c9c2c67df94c41fd0a496a057fc
GitHubdigicontributerdigicontributerdigicontributerdigicontributerdigicontributer <noreply@github.comesot@digibyte.ioesot@digibyte.ioesot@digibyte.ioesot@digibyte.ioesot@digibyte.io>
2021-07-22 21:51:53 +0800

    Merge 8.22: src/zmq/zmqpublishnotifier.h: Merged file with upstream

--- src/zmq/zmqpublishnotifier.h
+++ src/zmq/zmqpublishnotifier.h
@@ -13,7 +13,7 @@ class CBlockIndex;
 class CZMQAbstractPublishNotifier : public CZMQAbstractNotifier
 {
 private:
-    uint32_t nSequence; //!< upcounting per message sequence number
+    uint32_t nSequence {0U}; //!< upcounting per message sequence number
 
 public:
 
@@ -23,7 +23,7 @@ public:
           * data
           * message sequence number
     */
-    bool SendMessage(const char *command, const void* data, size_t size);
+    bool SendZmqMessage(const char *command, const void* data, size_t size);
 
     bool Initialize(void *pcontext) override;
     void Shutdown() override;
@@ -51,6 +51,15 @@ class CZMQPublishRawTransactionNotifier : public CZMQAbstractPublishNotifier
 {
 public:
     bool NotifyTransaction(const CTransaction &transaction) override;
+};
+
+class CZMQPublishSequenceNotifier : public CZMQAbstractPublishNotifier
+{
+public:
+    bool NotifyBlockConnect(const CBlockIndex *pindex) override;
+    bool NotifyBlockDisconnect(const CBlockIndex *pindex) override;
+    bool NotifyTransactionAcceptance(const CTransaction &transaction, uint64_t mempool_sequence) override;
+    bool NotifyTransactionRemoval(const CTransaction &transaction, uint64_t mempool_sequence) override;
 };
 
 #endif // DIGIBYTE_ZMQ_ZMQPUBLISHNOTIFIER_H
