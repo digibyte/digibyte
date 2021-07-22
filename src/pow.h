4d096d4d9b657bc8bb82f1a943b8d139703e1559
GitHubdigicontributerdigicontributerdigicontributerdigicontributerdigicontributer <noreply@github.comesot@digibyte.ioesot@digibyte.ioesot@digibyte.ioesot@digibyte.ioesot@digibyte.io>
2021-07-22 21:51:53 +0800

    Merge 8.22: src/pow.h: Merged file with upstream

--- src/pow.h
+++ src/pow.h
@@ -1,6 +1,6 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2019 The Bitcoin Core developers
-// Copyright (c) 2014-2019 The DigiByte Core developers
+// Copyright (c) 2009-2020 The Bitcoin Core developers
+// Copyright (c) 2014-2020 The DigiByte Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
