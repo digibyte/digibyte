fb5423c67e89a7b70b671f3b12287a9c91cfc761
GitHubdigicontributerdigicontributerdigicontributerdigicontributerdigicontributer <noreply@github.comesot@digibyte.ioesot@digibyte.ioesot@digibyte.ioesot@digibyte.ioesot@digibyte.io>
2021-07-22 21:51:53 +0800

    Merge 8.22: src/util/moneystr.cpp: Added and modified file from upstream

--- src/util/moneystr.cpp
+++ src/util/moneystr.cpp
@@ -4,24 +4,28 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include <utilmoneystr.h>
+#include <util/moneystr.h>
 
-#include <primitives/transaction.h>
 #include <tinyformat.h>
-#include <utilstrencodings.h>
+#include <util/strencodings.h>
+#include <util/string.h>
 
-std::string FormatMoney(const CAmount& n)
+std::string FormatMoney(const CAmount n)
 {
     // Note: not using straight sprintf here because we do NOT want
     // localized number formatting.
-    int64_t n_abs = (n > 0 ? n : -n);
-    int64_t quotient = n_abs/COIN;
-    int64_t remainder = n_abs%COIN;
+    static_assert(COIN > 1);
+    int64_t quotient = n / COIN;
+    int64_t remainder = n % COIN;
+    if (n < 0) {
+        quotient = -quotient;
+        remainder = -remainder;
+    }
     std::string str = strprintf("%d.%08d", quotient, remainder);
 
     // Right-trim excess zeros before the decimal point:
     int nTrim = 0;
-    for (int i = str.size()-1; (str[i] == '0' && isdigit(str[i-2])); --i)
+    for (int i = str.size()-1; (str[i] == '0' && IsDigit(str[i-2])); --i)
         ++nTrim;
     if (nTrim)
         str.erase(str.size()-nTrim, nTrim);
@@ -32,40 +36,41 @@ std::string FormatMoney(const CAmount& n)
 }
 
 
-bool ParseMoney(const std::string& str, CAmount& nRet)
+bool ParseMoney(const std::string& money_string, CAmount& nRet)
 {
-    return ParseMoney(str.c_str(), nRet);
-}
+    if (!ValidAsCString(money_string)) {
+        return false;
+    }
+    const std::string str = TrimString(money_string);
+    if (str.empty()) {
+        return false;
+    }
 
-bool ParseMoney(const char* pszIn, CAmount& nRet)
-{
     std::string strWhole;
     int64_t nUnits = 0;
-    const char* p = pszIn;
-    while (isspace(*p))
-        p++;
+    const char* p = str.c_str();
     for (; *p; p++)
     {
         if (*p == '.')
         {
             p++;
-            int64_t nMult = CENT*10;
-            while (isdigit(*p) && (nMult > 0))
+            int64_t nMult = COIN / 10;
+            while (IsDigit(*p) && (nMult > 0))
             {
                 nUnits += nMult * (*p++ - '0');
                 nMult /= 10;
             }
             break;
         }
-        if (isspace(*p))
-            break;
-        if (!isdigit(*p))
+        if (IsSpace(*p))
+            return false;
+        if (!IsDigit(*p))
             return false;
         strWhole.insert(strWhole.end(), *p);
     }
-    for (; *p; p++)
-        if (!isspace(*p))
-            return false;
+    if (*p) {
+        return false;
+    }
     if (strWhole.size() > 10) // guard against 63 bit overflow
         return false;
     if (nUnits < 0 || nUnits > COIN)
