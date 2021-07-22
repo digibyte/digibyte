f8225e90d1d0bb88c16447504c6792bbd6ab398a
GitHubdigicontributerdigicontributerdigicontributerdigicontributerdigicontributer <noreply@github.comesot@digibyte.ioesot@digibyte.ioesot@digibyte.ioesot@digibyte.ioesot@digibyte.io>
2021-07-22 21:51:53 +0800

    Merge 8.22: src/qt/locale/digibyte_bs.ts: Merged file with upstream

--- src/qt/locale/digibyte_bs.ts
+++ src/qt/locale/digibyte_bs.ts
@@ -1,7 +1,103 @@
 <TS language="bs" version="2.1">
 <context>
     <name>AddressBookPage</name>
-    </context>
+    <message>
+        <source>Right-click to edit address or label</source>
+        <translation>Desni klik za uređivanje adrese ili oznake</translation>
+    </message>
+    <message>
+        <source>Create a new address</source>
+        <translation>Napravite novu adresu</translation>
+    </message>
+    <message>
+        <source>&amp;New</source>
+        <translation>&amp;Nova</translation>
+    </message>
+    <message>
+        <source>Copy the currently selected address to the system clipboard</source>
+        <translation>Kopirajte trenutno odabranu adresu u sistemski meduspremnik</translation>
+    </message>
+    <message>
+        <source>&amp;Copy</source>
+        <translation>&amp;Kopirajte</translation>
+    </message>
+    <message>
+        <source>C&amp;lose</source>
+        <translation>&amp;Zatvorite</translation>
+    </message>
+    <message>
+        <source>Delete the currently selected address from the list</source>
+        <translation>Izbrišite trenutno odabranu odresu sa liste</translation>
+    </message>
+    <message>
+        <source>Enter address or label to search</source>
+        <translation>Unesite adresu ili oznaku za pretragu</translation>
+    </message>
+    <message>
+        <source>Export the data in the current tab to a file</source>
+        <translation>Izvezite podatke trenutne kartice u datoteku</translation>
+    </message>
+    <message>
+        <source>&amp;Export</source>
+        <translation>&amp;Izvezite</translation>
+    </message>
+    <message>
+        <source>&amp;Delete</source>
+        <translation>Iz&amp;brišite</translation>
+    </message>
+    <message>
+        <source>Choose the address to send coins to</source>
+        <translation>Izaberite adresu na koju ćete poslati novac</translation>
+    </message>
+    <message>
+        <source>Choose the address to receive coins with</source>
+        <translation>Izaberite adresu na koju ćete primiti novac</translation>
+    </message>
+    <message>
+        <source>C&amp;hoose</source>
+        <translation>&amp;Izaberite</translation>
+    </message>
+    <message>
+        <source>Sending addresses</source>
+        <translation>Adrese pošiljalaca</translation>
+    </message>
+    <message>
+        <source>Receiving addresses</source>
+        <translation>Adrese primalaca</translation>
+    </message>
+    <message>
+        <source>These are your DigiByte addresses for sending payments. Always check the amount and the receiving address before sending coins.</source>
+        <translation>Ovo su vaše DigiByte adrese za slanje novca. Uvijek provjerite iznos i adresu primaoca prije slanja novca.</translation>
+    </message>
+    <message>
+        <source>&amp;Copy Address</source>
+        <translation>&amp;Kopirajte adresu</translation>
+    </message>
+    <message>
+        <source>Copy &amp;Label</source>
+        <translation>Kopirajte &amp;ozanku</translation>
+    </message>
+    <message>
+        <source>&amp;Edit</source>
+        <translation>&amp;Uredite</translation>
+    </message>
+    <message>
+        <source>Export Address List</source>
+        <translation>Izvezite listu adresa</translation>
+    </message>
+    <message>
+        <source>Comma separated file (*.csv)</source>
+        <translation>Datoteka podataka odvojenih zarezima (*.csv)</translation>
+    </message>
+    <message>
+        <source>Exporting Failed</source>
+        <translation>Izvoz neuspješan</translation>
+    </message>
+    <message>
+        <source>There was an error trying to save the address list to %1. Please try again.</source>
+        <translation>Došlo je do greške kod spašavanja liste adresa na %1. Molimo pokušajte ponovo.</translation>
+    </message>
+</context>
 <context>
     <name>AddressTableModel</name>
     </context>
@@ -13,13 +109,15 @@
     </context>
 <context>
     <name>DigiByteGUI</name>
-    <message>
-        <source>DigiByte</source>
-        <translation>DigiByte</translation>
-    </message>
     </context>
 <context>
     <name>CoinControlDialog</name>
+    </context>
+<context>
+    <name>CreateWalletActivity</name>
+    </context>
+<context>
+    <name>CreateWalletDialog</name>
     </context>
 <context>
     <name>EditAddressDialog</name>
@@ -32,6 +130,10 @@
     </context>
 <context>
     <name>Intro</name>
+    <message>
+        <source>DigiByte</source>
+        <translation>DigiByte</translation>
+    </message>
     </context>
 <context>
     <name>ModalOverlay</name>
@@ -40,10 +142,16 @@
     <name>OpenURIDialog</name>
     </context>
 <context>
+    <name>OpenWalletActivity</name>
+    </context>
+<context>
     <name>OptionsDialog</name>
     </context>
 <context>
     <name>OverviewPage</name>
+    </context>
+<context>
+    <name>PSBTOperationsDialog</name>
     </context>
 <context>
     <name>PaymentServer</name>
@@ -53,9 +161,6 @@
     </context>
 <context>
     <name>QObject</name>
-    </context>
-<context>
-    <name>QObject::QObject</name>
     </context>
 <context>
     <name>QRImageWidget</name>
@@ -87,9 +192,6 @@
     </message>
     </context>
 <context>
-    <name>SendConfirmationDialog</name>
-    </context>
-<context>
     <name>ShutdownWindow</name>
     </context>
 <context>
@@ -102,9 +204,6 @@
         <source>Alt+P</source>
         <translation>Alt+P</translation>
     </message>
-    </context>
-<context>
-    <name>SplashScreen</name>
     </context>
 <context>
     <name>TrafficGraphWidget</name>
@@ -140,9 +239,20 @@
         <source>This year</source>
         <translation>Ove godine</translation>
     </message>
+    <message>
+        <source>Comma separated file (*.csv)</source>
+        <translation>Datoteka podataka odvojenih zarezima (*.csv)</translation>
+    </message>
+    <message>
+        <source>Exporting Failed</source>
+        <translation>Izvoz neuspješan</translation>
+    </message>
     </context>
 <context>
     <name>UnitDisplayStatusBarControl</name>
+    </context>
+<context>
+    <name>WalletController</name>
     </context>
 <context>
     <name>WalletFrame</name>
@@ -152,12 +262,16 @@
     </context>
 <context>
     <name>WalletView</name>
+    <message>
+        <source>&amp;Export</source>
+        <translation>&amp;Izvezite</translation>
+    </message>
+    <message>
+        <source>Export the data in the current tab to a file</source>
+        <translation>Izvezite podatke trenutne kartice u datoteku</translation>
+    </message>
     </context>
 <context>
     <name>digibyte-core</name>
-    <message>
-        <source>DigiByte Core</source>
-        <translation>DigiByte Jezrga</translation>
-    </message>
     </context>
 </TS>
