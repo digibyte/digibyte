c5fcdc9bd2eef6e5b84005e575968314bdcec73c
GitHubdigicontributerdigicontributerdigicontributerdigicontributerdigicontributer <noreply@github.comesot@digibyte.ioesot@digibyte.ioesot@digibyte.ioesot@digibyte.ioesot@digibyte.io>
2021-07-22 21:51:53 +0800

    Merge 8.22: src/qt/createwalletdialog.h: Added and modified file from upstream

---
+++ src/qt/createwalletdialog.h
@@ -0,0 +1,40 @@
+// Copyright (c) 2019 The DigiByte Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef DIGIBYTE_QT_CREATEWALLETDIALOG_H
+#define DIGIBYTE_QT_CREATEWALLETDIALOG_H
+
+#include <QDialog>
+
+class ExternalSigner;
+class WalletModel;
+
+namespace Ui {
+    class CreateWalletDialog;
+}
+
+/** Dialog for creating wallets
+ */
+class CreateWalletDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit CreateWalletDialog(QWidget* parent);
+    virtual ~CreateWalletDialog();
+
+    void setSigners(const std::vector<ExternalSigner>& signers);
+
+    QString walletName() const;
+    bool isEncryptWalletChecked() const;
+    bool isDisablePrivateKeysChecked() const;
+    bool isMakeBlankWalletChecked() const;
+    bool isDescriptorWalletChecked() const;
+    bool isExternalSignerChecked() const;
+
+private:
+    Ui::CreateWalletDialog *ui;
+};
+
+#endif // DIGIBYTE_QT_CREATEWALLETDIALOG_H
