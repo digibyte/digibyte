66f8de72da4fe7f8a4f466175fff83d3d591f1c1
Yoshi <yoshi@jaeger.berlin>
2021-07-27 13:15:42 +0200

    Fix: Properly added LOCK for Stempool Mutex to the required CriticalSections

--- src/validation.cpp
+++ src/validation.cpp
@@ -2854,7 +2854,7 @@ bool CChainState::ActivateBestChain(BlockValidationState& state, std::shared_ptr
         {
             LOCK(cs_main);
             // Lock transaction pool for at least as long as it takes for connectTrace to be consumed
-            LOCK(MempoolMutex());
+            LOCK2(MempoolMutex(), StempoolMutex());
             CBlockIndex* starting_tip = m_chain.Tip();
             bool blocks_connected = false;
             do {
@@ -3006,7 +3006,7 @@ bool CChainState::InvalidateBlock(BlockValidationState& state, CBlockIndex* pind
         LOCK(cs_main);
         // Lock for as long as disconnectpool is in scope to make sure MaybeUpdateMempoolForReorg is
         // called after DisconnectTip without unlocking in between
-        LOCK(MempoolMutex());
+        LOCK2(MempoolMutex(), StempoolMutex());
         if (!m_chain.Contains(pindex)) break;
         pindex_was_in_chain = true;
         CBlockIndex *invalid_walk_tip = m_chain.Tip();
