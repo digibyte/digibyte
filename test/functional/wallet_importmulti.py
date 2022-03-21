458400491d795d835836f35fa8ce7b0596562440
Yoshi Jäger <github@digibyte.org>
2022-03-21 13:37:26 +0100

    fix: functional: wallet_importmulti.py: updated test vectors

--- test/functional/wallet_importmulti.py
+++ test/functional/wallet_importmulti.py
@@ -575,9 +575,10 @@ class ImportMultiTest(DigiByteTestFramework):
 
         # Test ranged descriptor fails if range is not specified
         xpriv = "tprv8ZgxMBicQKsPeuVhWwi6wuMQGfPKi9Li5GtX35jVNknACgqe3CY4g5xgkfDDJcmtF7o1QnxWDRYw4H5P26PXq7sbcUkEqeR4fg3Kxp2tigg"
-        addresses = ["2N7yv4p8G8yEaPddJxY41kPihnWvs39qCMf", "2MsHxyb2JS3pAySeNUsJ7mNnurtpeenDzLA"] # hdkeypath=m/0'/0'/0' and 1'
-        addresses += ["bcrt1qrd3n235cj2czsfmsuvqqpr3lu6lg0ju7scl8gn", "bcrt1qfqeppuvj0ww98r6qghmdkj70tv8qpchehegrg8"] # wpkh subscripts corresponding to the above addresses
+        addresses = ["yb48vjS8NsHWbMpTb3QAbNUeYGW3F8eRas", "yLNBqWLAfws7BAqX7NeGcMYrcePpo44GCY"] # hdkeypath=m/0'/0'/0' and 1'
+        addresses += ["dgbrt1qrd3n235cj2czsfmsuvqqpr3lu6lg0ju77h2ulg", "dgbrt1qfqeppuvj0ww98r6qghmdkj70tv8qpcheekaclu"] # wpkh subscripts corresponding to the above addresses
         desc = "sh(wpkh(" + xpriv + "/0'/0'/*'" + "))"
+
         self.log.info("Ranged descriptor import should fail without a specified range")
         self.test_importmulti({"desc": descsum_create(desc),
                                "timestamp": "now"},
@@ -613,8 +614,8 @@ class ImportMultiTest(DigiByteTestFramework):
                               success=False, error_code=-8, error_message='Range is too large')
 
         # Test importing a descriptor containing a WIF private key
-        wif_priv = "cTe1f5rdT8A8DFgVWTjyPwACsDPJM9ff4QngFxUixCSvvbg1x6sh"
-        address = "2MuhcG52uHPknxDgmGPsV18jSHFBnnRgjPg"
+        wif_priv = "efJ5gWnzWEngkYAV6BqwZjV8x8GUopWXZJbE83kEnNZyUnMgHV4g"
+        address = "yRNbfPsvBYmb8QTyAdAjjMSfiqgi4J9rXC"
         desc = "sh(wpkh(" + wif_priv + "))"
         self.log.info("Should import a descriptor with a WIF private key as spendable")
         self.test_importmulti({"desc": descsum_create(desc),
@@ -745,7 +746,7 @@ class ImportMultiTest(DigiByteTestFramework):
         self.log.info("Bech32m addresses and descriptors cannot be imported")
         self.test_importmulti(
             {
-                "scriptPubKey": {"address": "bcrt1p0xlxvlhemja6c4dqv22uapctqupfhlxm9h8z3k2e72q4k9hcz7vqc8gma6"},
+                "scriptPubKey": {"address": "dgbrt1p0xlxvlhemja6c4dqv22uapctqupfhlxm9h8z3k2e72q4k9hcz7vq64ly68"},
                 "timestamp": "now",
             },
             success=False,
@@ -856,12 +857,13 @@ class ImportMultiTest(DigiByteTestFramework):
         assert_equal(wrpc.getwalletinfo()["private_keys_enabled"], False)
         xpub = "tpubDAXcJ7s7ZwicqjprRaEWdPoHKrCS215qxGYxpusRLLmJuT69ZSicuGdSfyvyKpvUNYBW1s2U3NSrT6vrCYB9e6nZUEvrqnwXPF8ArTCRXMY"
         addresses = [
-            'bcrt1qtmp74ayg7p24uslctssvjm06q5phz4yrxucgnv', # m/0'/0'/0
-            'bcrt1q8vprchan07gzagd5e6v9wd7azyucksq2xc76k8', # m/0'/0'/1
-            'bcrt1qtuqdtha7zmqgcrr26n2rqxztv5y8rafjp9lulu', # m/0'/0'/2
-            'bcrt1qau64272ymawq26t90md6an0ps99qkrse58m640', # m/0'/0'/3
-            'bcrt1qsg97266hrh6cpmutqen8s4s962aryy77jp0fg0', # m/0'/0'/4
+            'dgbrt1qtmp74ayg7p24uslctssvjm06q5phz4yrgndnyh', # m/0'/0'/0
+            'dgbrt1q8vprchan07gzagd5e6v9wd7azyucksq2ghtppu', # m/0'/0'/1
+            'dgbrt1qtuqdtha7zmqgcrr26n2rqxztv5y8rafj0228g8', # m/0'/0'/2
+            'dgbrt1qau64272ymawq26t90md6an0ps99qkrse6gwpz5', # m/0'/0'/3
+            'dgbrt1qsg97266hrh6cpmutqen8s4s962aryy77uw6jl5', # m/0'/0'/4
         ]
+
         result = wrpc.importmulti(
             [{
                 'desc': descsum_create('wpkh([80002067/0h/0h]' + xpub + '/*)'),
