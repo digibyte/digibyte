ad7ca6b5f16a6f0f14b41730e20b4957f9b808a8
Yoshi Jäger <github@digibyte.org>
2022-03-26 09:28:58 +0100

    functional: license header altered

--- test/functional/p2p_leak_tx.py
+++ test/functional/p2p_leak_tx.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright (c) 2017-2021 The DigiByte Core developers
+# Copyright (c) 2021-2022 The DigiByte Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 """Test that we don't leak txs to inbound peers that we haven't yet announced to"""
